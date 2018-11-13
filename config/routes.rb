a59ad621d439e11cea58bbca20b4b5cdcaf69e42
Henrique Aguilar <henriaguilar@gmail.com>
2018-11-13 17:00:03 -0200

    controller and views created

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'availabilities/new'
+  get 'availabilities/create'
+  get 'availabilities/show'
+  get 'availabilities/index'
+  get 'availabilities/edit'
+  get 'availabilities/update'
+  get 'availabilities/destroy'
   devise_for :users
   root to: 'pages#home'
   resources :users, only: [:show] do
     resources :students
     resources :senseis do
       resources :sensei_subjects, only: [:create, :new, :destroy, :index]
+      resources :availabitities, only: [:new, :create, :index, :edit, :update, :destroy]
     end
   end
   resources :subjects, only: [:index]
