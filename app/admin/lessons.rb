fccf514b1bd88006ad155b51900d7b7c203aeaaa
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-11-13 17:39:21 -0300

    Admin setup done

--- app/admin/lessons.rb
+++ app/admin/lessons.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Lesson do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-permit_params :reserved, :video, :sensei_id, :students_id
+# permit_params :reserved, :video, :sensei_id, :students_id
 # permit_params :list, :of, :attributes, :on, :model
 #
 # or
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,15 +1,25 @@
 ActiveAdmin.register User do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+  index  do
+    selectable_column
+    column :id
+    column :email
+    column :full_name
+    column :created_at
+    column :admin
+    column :sensei
+    column :updated_at
+    actions
+  end
+  form do |f|
+    f.inputs "Identity" do
+      f.input :full_name
+      f.input :email
+      end
+    f.inputs "Admin" do
+      f.input :admin
+      end
+    f.actions
+    end
 
+    permit_params :full_name, :email, :admin
 end
